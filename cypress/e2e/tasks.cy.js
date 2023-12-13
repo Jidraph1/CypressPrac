2f90f8f81605f99369d15d618e686864e894c600
Jidraph! <jidraphkimachia1@gmail.com>
2023-12-13 09:16:38 +0300

    input validation test case

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -6,5 +6,34 @@ describe('Tasks management', () =>{
         cy.visit('http://localhost:5173/');
         cy.contains("Add Task").click()
         cy.get('.backdrop').click({force: true});
+        cy.get('.backdrop').should('not.exist')
+        cy.get('.modal').should('not.exist')
+        
+        cy.contains("Add Task").click()
+        cy.contains('Cancel').click()
+        cy.get('.backdrop').should('not.exist')
+        cy.get('.modal').should('not.exist')
+    })
+
+    it('should create a new task', () =>{
+        cy.visit('http://localhost:5173/');
+        cy.contains('Add Task').click()
+        cy.get('#title').type('This a new task')
+        cy.get('#summary').type('This is the description')
+        cy.get('.modal').contains('Add Task').click()
+
+        cy.get('.backdrop').should('not.exist')
+        cy.get('.modal').should('not.exist')
+        cy.get('.task').should('have.length', 1)
+        cy.get('.task h2').contains('This a new task')
+        cy.get('.task p').contains('This is the description')
+    })
+
+    it('should validate the users input', ()=>{
+        cy.visit('http://localhost:5173/');
+        cy.contains('Add Task').click()
+        cy.get('.modal').contains('Add Task').click()
+        cy.get('.error-message').should('exist')
+
     })
 })
